ec6f77f8d4735d1b48467d4ad991baa6295f53f0
SID12g <sid12g@naver.com>
2024-04-13 21:18:42 +0900

    fix: default setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://dote-advocate.vercel.app"),
   title: "advocate",
   description: "개별화교육계획을 수립하는 새로운 방법",
   openGraph: {
     title: "advocate",
     description: "개별화교육계획을 수립하는 새로운 방법",
-    url: "",
+    url: "https://dote-advocate.vercel.app",
     images: [{ url: "" }],
     locale: "ko_KR",
     type: "website",
