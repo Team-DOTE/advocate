63421ff7c239b5f9d15a257671ca37ced650a624
손희창 <mikeylili73@dimigo.hs.kr>
2024-04-16 22:51:24 +0900

    feat: iep page

--- src/app/class/[id]/iep/page.tsx
+++ src/app/class/[id]/iep/page.tsx
@@ -1,3 +1,35 @@
+import styles from "@/app/class/[id]/iep/page.module.css";
+import Title from "@/components/title/title";
+import Link from "next/link";
 export default function Iep() {
-  return <div>iep hggjhgj</div>;
+  return (
+    <div style={{ width: "100%" }}>
+      <Title title="IEP 생성" />
+      <div className={styles.container}>
+        <form method="POST" action="">
+          <p className={styles.content}>IEP 생성 대상 학생을 선택해주세요.</p>
+          <input placeholder="학생을 선택해주세요." className={styles.input} />
+          <p className={styles.content}>IEP 생성 과목을 선택해주세요.</p>
+          <input placeholder="과목을 선택해주세요." className={styles.input} />
+          <p className={styles.content}>
+            해당 과목에 대한 대상 학생의 현행 수준을 자세히 작성해주세요.
+          </p>
+          <input
+            placeholder="학생의 특성을 자세히 작성해주세요."
+            className={styles.input}
+          />
+          <p className={styles.content}>
+            해당 과목의 교육 목적 및 기대 효과를 작성해주세요.
+          </p>
+          <input
+            placeholder="학습 목적 및 기대 효과를 작성해주세요."
+            className={styles.input}
+          />
+          <button type="submit" className={styles.button}>
+            과목 추가
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
