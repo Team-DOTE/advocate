24da8612f7576ca400515571485ed3563ce890c2
mikeylili <a01073111145@gmail.com>
2024-05-22 23:00:01 +0900

    feat: print

---
+++ src/app/(teacher)/class/[id]/iep/print/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { useRef, useEffect, useState } from "react";
+import ReactToPrint from "react-to-print";
+import Report from "@/components/class/Report/Report";
+const App = () => {
+  const [content, setContent] = useState("");
+  const ref = useRef<HTMLDivElement>(null); 
+
+  useEffect(() => {
+    fetch("https://baconipsum.com/api/?type=all-meat&start-with-lorem=1")
+      .then((res) => res.json())
+      .then((data) => setContent(data));
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <h2>리포트 보기</h2>
+        <ReactToPrint
+          trigger={() => <button>리포트 출력</button>}
+          content={() => ref.current}
+        />
+      </div>
+      <div style={{display:"none"}}>
+        <Report ref={ref} content={content} />
+      </div>
+    </div>
+  );
+};
+export default App;
