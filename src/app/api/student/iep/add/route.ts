017e8a80ed16c78b5a423426d31535bd5f417fa0
SID12g <sid12g@naver.com>
2024-05-25 19:13:08 +0900

    feat: iep result by base api

---
+++ src/app/api/student/iep/add/route.ts
@@ -0,0 +1,34 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { connectDB } from "@/utils/database";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export async function POST(request: NextRequest, response: NextResponse) {
+  const date = new Date();
+  const session: any = await getServerSession(authOptions);
+  const formData = await request.formData();
+  const student = formData.get("student");
+  const subject = formData.get("subject");
+  const level = formData.get("level");
+  const purpose = formData.get("purpose");
+  const classid = formData.get("classid");
+  const iep = {
+    date,
+    student,
+    subject,
+    level,
+    purpose,
+    classid,
+  };
+
+  try {
+    let db = (await connectDB).db("advocate");
+    await db.collection("iep").insertOne(iep);
+    let iepdata = await db
+      .collection("iep")
+      .findOne({ date, student, subject, classid });
+    return Response.json({ status: 200, success: true, iepdata });
+  } catch (error) {
+    return Response.json({ status: 500, error });
+  }
+}
---
+++ src/components/class/iep/result/result.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import styles from "@/components/class/iep/result/result.module.css";
+import ClassWrap from "@/components/class/wrap/wrap";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import ReactToPrint from "react-to-print";
+import print from "@/../public/icons/print.svg";
+import axios from "axios";
+import IepReport from "../report/report";
+
+interface Iep {
+  _id: any;
+  date: Date;
+  student: string;
+  subject: string;
+  level: string;
+  purpose: string;
+  classid: string;
+}
+
+const IepResult = ({
+  params,
+  url,
+}: {
+  params: { iid: string };
+  url?: string;
+}) => {
+  const [content, setContent] = useState<Iep>({
+    _id: null,
+    date: new Date(),
+    student: "",
+    subject: "",
+    level: "",
+    purpose: "",
+    classid: "",
+  });
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    async function fetch() {
+      const response = await axios.get(
+        `${url}/api/student/iep/get?iid=${params.iid}`
+      );
+      // fetch 함수 아래에 setUsers를 해주어야 한다.
+      console.log(response.data);
+      setContent(response.data.iepdata);
+    }
+    fetch();
+  }, [params.iid, url]);
+  return (
+    <ClassWrap>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <p className={styles.title}>IEP 결과</p>
+          <ReactToPrint
+            trigger={() => (
+              <Image className={styles.icon} src={print} alt="print icon" />
+            )}
+            content={() => ref.current}
+          />
+        </div>
+        <div style={{ marginTop: -60 }}>
+          <IepReport ref={ref} content={content} />
+        </div>
+      </div>
+    </ClassWrap>
+  );
+};
+export default IepResult;
