160a589170a1cb759dc19b215390effbaef1455d
SID12g <sid12g@naver.com>
2024-05-27 18:26:29 +0900

    fix: parent messages ui size, flex direction of messages

--- src/app/(teacher)/class/[id]/messages/[mid]/page.tsx
+++ src/app/(teacher)/class/[id]/messages/[mid]/page.tsx
@@ -14,9 +14,24 @@ export default function Messages() {
         <Image className={styles.profile} src={profile0} alt="profile" />
         <p className={styles.name}>황석준 보호자</p>
       </div>
-      <ClassMessageDate date={"2024년 5월 24일 (금)"} />
-      <ClassMyMessage content={"안녕하세요? \n 안녕"} time={"오후 08:11"} />
-      <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+      <div className={styles.messages}>
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassMessageDate date={"2024년 5월 24일 (금)"} />
+        <ClassMyMessage content={"안녕하세요? \n 안녕"} time={"오후 08:11"} />
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassOtherMessage content={"야옹"} time={"오후 08:12"} />
+        <ClassMyMessage content={"안녕하세요? \n 안녕"} time={"오후 08:11"} />
+        <ClassMessageDate date={"2024년 5월 24일 (금)"} />
+      </div>
       <div className={styles.wrap}>
         <input className={styles.input}></input>
         <div className={styles.enter}>
