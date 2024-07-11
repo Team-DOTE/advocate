40dae1636b7f0440cd0df867d42a99c2634d606f
mikeylili <a01073111145@gmail.com>
2024-07-12 08:25:27 +0900

    fix: evaluateDDay

--- src/app/(teacher)/class/[id]/evaluate/[sid]/[eid]/page.tsx
+++ src/app/(teacher)/class/[id]/evaluate/[sid]/[eid]/page.tsx
@@ -18,8 +18,8 @@ export default async function EvaluateList({params}:{params:{id:string, sid:stri
     let evaluate:any = await db.collection("evaluate").findOne({ _id : evaluateObjectId});
     const date: any = new Date();
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
+    const month:any = String(date.getMonth() + 1).padStart(2, '0');
+    const day:any = String(date.getDate()).padStart(2, '0');
     const Converteddate: any = `${year}년 ${month}월 ${day}일`;
     const startyear = evaluate.startdate.slice(0, 4);
     const startmonth = evaluate.startdate.slice(6, 8);
@@ -27,10 +27,11 @@ export default async function EvaluateList({params}:{params:{id:string, sid:stri
     const endyear = evaluate.enddate.slice(0, 4);
     const endmonth = evaluate.enddate.slice(6, 8);
     const endday = evaluate.enddate.slice(10, 12);
-    const dyear = endyear - startyear;
-    const dmonth = endmonth - startmonth;
-    const dday = endday - startday;
+    const dyear = endyear - year;
+    const dmonth = endmonth - month;
+    const dday = endday - day;
     let did = (Converteddate == evaluate.dates[evaluate.dates.length - 1]) ? true : false;
+    let first = (evaluate.dates[0])? false : true;
     
     
     return(
@@ -40,8 +41,8 @@ export default async function EvaluateList({params}:{params:{id:string, sid:stri
             <div className={styles.evaluate_info}>
                 <div className={styles.level_wrap}>
                     <div className={styles.level_title}>최근 성취도</div>
-                    <div className={styles.level_date}>{evaluate.startdate}</div>
-                    <div className={styles.level_content}>{(did)?`${evaluate.content[evaluate.content.length - 1]}%`:`0%`}</div>
+                    <div className={styles.level_date}>{evaluate.dates[evaluate.dates.length - 1]}</div>
+                    <div className={styles.level_content}>{(!first)?`${evaluate.content[evaluate.content.length - 1]}%`:`0%`}</div>
                 </div>
                 <div className={styles.dates_wrap}>
                     <div className={styles.date_wrap}>
